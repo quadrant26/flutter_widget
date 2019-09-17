049b3fb813e2443766cc0f6cc5718898916fe191
quadrant26 <quadrant26@163.com>
2019-09-17 18:01:25 +0800

    MaterialApp Scaffold

---
+++ flutter_app01/lib/main_demo02.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: HomePage(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello, Flutter',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 40.0,
+          color: Colors.yellow
+        )
+      ),
+    );
+  }
+}
