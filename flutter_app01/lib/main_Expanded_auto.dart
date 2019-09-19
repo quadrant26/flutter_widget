f40c15b01d3f68c89afc5812c47fc6d082efb772
quadrant26 <quadrant26@163.com>
2019-09-19 11:39:04 +0800

    Expanded 左侧固定，自适应布局

---
+++ flutter_app01/lib/main_Expanded_auto.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'res/listData.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: LayoutDemo(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        IconContainer(Icons.home, color: Colors.red),
+        Expanded(
+          flex: 1,
+          child: IconContainer(Icons.search, color: Colors.black),
+        )
+      ],
+    );
+  }
+}
+
+class IconContainer extends StatelessWidget {
+
+  double size = 32.0;
+  Color color = Colors.red;
+  IconData icon;
+
+  IconContainer(this.icon, {this.color, this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      color: this.color,
+      child: Center(
+        child: Icon(this.icon, size: this.size, color: Colors.white)
+      )
+    );
+  }
+}
