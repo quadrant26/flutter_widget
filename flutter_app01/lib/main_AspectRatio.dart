e8181dfad1deecee536b745ee8afe0f45636cf6d
quadrant26 <quadrant26@163.com>
2019-09-19 16:42:51 +0800

    AspectRatio

---
+++ flutter_app01/lib/main_AspectRatio.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'res/listData.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: LayoutDemo(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width:300,
+      child: AspectRatio(
+        aspectRatio: 2.0/1.0,
+        child: Container(
+          color: Colors.red
+        )
+      )
+    );
+  }
+}
