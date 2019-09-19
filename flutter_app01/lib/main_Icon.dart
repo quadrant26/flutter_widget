81760aab3cf0493204f2e8df8716bf50e72857b2
quadrant26 <quadrant26@163.com>
2019-09-19 11:25:56 +0800

    Icon 组件

---
+++ flutter_app01/lib/main_Icon.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'res/listData.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: LayoutDemo(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconContainer(Icons.home);
+  }
+}
+
+class IconContainer extends StatelessWidget {
+
+  double size = 32.0;
+  Color color = Colors.red;
+  IconData icon;
+
+  IconContainer(this.icon, {this.color, this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      color: this.color,
+      child: Center(
+        child: Icon(Icons.home, size: this.size, color: Colors.white)
+      )
+    );
+  }
+}
