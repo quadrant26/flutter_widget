e1eccb8deb69a2fdfdbc9a17e4d8d0881ab6fdc8
quadrant26 <quadrant26@163.com>
2019-09-19 16:42:17 +0800

    Stack alignment: Alignment(1, 0.5)

---
+++ flutter_app01/lib/main_Stack_Alignment_args.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'res/listData.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: LayoutDemo(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment(1, 0.5),
+        children: <Widget>[
+          Container(
+            height: 400,
+            width: 300,
+            color: Colors.red
+          ),
+          Text(
+            '我是一个文本1',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white
+            )
+          )
+        ],
+      )
+    );
+  }
+}
