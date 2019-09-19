abc06ac4ce447f93120796ede1132be8b2b397ae
quadrant26 <quadrant26@163.com>
2019-09-19 16:42:42 +0800

    AspectRatio auto

---
+++ flutter_app01/lib/main_AspectRatio_auto.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'res/listData.dart';
+
+void main() => runApp( MyApp() );
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: LayoutDemo(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 3.0/1.0,
+      child: Container(
+        color: Colors.red
+      )
+    );
+  }
+}
