40f4a2fa53962b9f25db54c76c44b37205bcea06
quadrant26 <quadrant26@163.com>
2019-09-20 17:25:57 +0800

    StatefulWidget TabController

--- flutter_app01/lib/pages/TabBarController.dart
+++ flutter_app01/lib/pages/TabBarController.dart
@@ -5,13 +5,55 @@ class TabBarControllerPage extends StatefulWidget {
   _TabBarControllerPageState createState() => _TabBarControllerPageState();
 }
 
-class _TabBarControllerPageState extends State<TabBarControllerPage> {
+class _TabBarControllerPageState extends State<TabBarControllerPage> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(
+      vsync: this,
+      length: 2
+    );
+
+    _tabController.addListener( (){
+      print(_tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TabBarController')
+        title: Text('TabBarController'),
+        bottom: TabBar(
+          controller: this._tabController, // 
+          tabs: <Widget>[
+            Tab(text: '热销'),
+            Tab(text: '推荐'),
+          ],
+        )
       ),
+      body: TabBarView(
+        controller: this._tabController, // 
+        children: <Widget>[
+          Center(
+            child: Text('热销'),
+          ),
+          Center(
+            child: Text('推荐'),
+          ),
+        ],
+      )
     );
   }
 }
