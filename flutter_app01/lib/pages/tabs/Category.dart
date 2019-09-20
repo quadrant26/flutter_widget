9b02d0a381da9622a9e8b57a3699d1e39d4edbd2
quadrant26 <quadrant26@163.com>
2019-09-20 17:09:18 +0800

    TabBar isScrollable: true

--- flutter_app01/lib/pages/tabs/Category.dart
+++ flutter_app01/lib/pages/tabs/Category.dart
@@ -10,20 +10,28 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 9,
       child: Scaffold(
         appBar: AppBar(
           title: Row(
             children: <Widget>[
               Expanded(
                 child: TabBar(
+                  isScrollable: true,
                   indicatorColor: Colors.red,
                   labelColor: Colors.red,
                   unselectedLabelColor: Colors.white,
                   indicatorSize: TabBarIndicatorSize.label,
                   tabs: <Widget>[
-                    Tab(text: '热销'),
-                    Tab(text: '推荐')
+                    Tab(text: '热销1'),
+                    Tab(text: '推荐2'),
+                    Tab(text: '推荐3'),
+                    Tab(text: '推荐4'),
+                    Tab(text: '推荐5'),
+                    Tab(text: '推荐6'),
+                    Tab(text: '推荐7'),
+                    Tab(text: '推荐8'),
+                    Tab(text: '推荐9'),
                   ],
                 )
               )
@@ -60,6 +68,97 @@ class _CategoryPageState extends State<CategoryPage> {
                 ),
                 ListTile(
                   title: Text('第二个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第三个Tab'),
+                ),
+                ListTile(
+                  title: Text('第三个Tab'),
+                ),
+                ListTile(
+                  title: Text('第三个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第四个Tab'),
+                ),
+                ListTile(
+                  title: Text('第四个Tab'),
+                ),
+                ListTile(
+                  title: Text('第四个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第五个Tab'),
+                ),
+                ListTile(
+                  title: Text('第五个Tab'),
+                ),
+                ListTile(
+                  title: Text('第五个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第六个Tab'),
+                ),
+                ListTile(
+                  title: Text('第六个Tab'),
+                ),
+                ListTile(
+                  title: Text('第六个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第七个Tab'),
+                ),
+                ListTile(
+                  title: Text('第七个Tab'),
+                ),
+                ListTile(
+                  title: Text('第七个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第八个Tab'),
+                ),
+                ListTile(
+                  title: Text('第八个Tab'),
+                ),
+                ListTile(
+                  title: Text('第八个Tab'),
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第九个Tab'),
+                ),
+                ListTile(
+                  title: Text('第九个Tab'),
+                ),
+                ListTile(
+                  title: Text('第九个Tab'),
                 )
               ],
             )
