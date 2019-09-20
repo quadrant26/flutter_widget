c4a10114421ffbd6bc3f6f3dd57b9d8356cd99b7
quadrant26 <quadrant26@163.com>
2019-09-20 16:43:34 +0800

    TabBar 设置

--- flutter_app01/lib/pages/AppBarDemo.dart
+++ flutter_app01/lib/pages/AppBarDemo.dart
@@ -5,12 +5,55 @@ class AppBarDemoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('AppBarDemoPage')
-      ),
-      body: Container(
-        child: Text('AppBarDemoPage'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('AppBarDemoPage'),
+          centerTitle: true, // 标题居中显示
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab( text: '热门'),
+              Tab( text: '推荐')
+            ],
+          )
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第一个tab')
+                ),
+                ListTile(
+                  title: Text('第一个tab')
+                ),
+                ListTile(
+                  title: Text('第一个tab')
+                ),
+                ListTile(
+                  title: Text('第一个tab')
+                ),
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第二个tab')
+                ),
+                ListTile(
+                  title: Text('第二个tab')
+                ),
+                ListTile(
+                  title: Text('第二个tab')
+                ),
+                ListTile(
+                  title: Text('第二个tab')
+                ),
+              ],
+            ),
+          ],
+        )
       )
     );
   }
--- flutter_app01/lib/pages/tabs/Category.dart
+++ flutter_app01/lib/pages/tabs/Category.dart
@@ -9,19 +9,63 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        RaisedButton(
-          child: Text('跳转到列表单页面'),
-          onPressed: (){
-            Navigator.pushNamed(context, '/form');
-          },
-          color: Theme.of(context).accentColor,
-          textTheme: ButtonTextTheme.primary,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Expanded(
+                child: TabBar(
+                  indicatorColor: Colors.red,
+                  labelColor: Colors.red,
+                  unselectedLabelColor: Colors.white,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  tabs: <Widget>[
+                    Tab(text: '热销'),
+                    Tab(text: '推荐')
+                  ],
+                )
+              )
+            ]
+          )
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('跳转到列表单页面'),
+                      onPressed: (){
+                        Navigator.pushNamed(context, '/form');
+                      },
+                      color: Theme.of(context).accentColor,
+                      textTheme: ButtonTextTheme.primary,
+                    )
+                  ],
+                )
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('第二个Tab'),
+                ),
+                ListTile(
+                  title: Text('第二个Tab'),
+                ),
+                ListTile(
+                  title: Text('第二个Tab'),
+                )
+              ],
+            )
+          ],
         )
-      ],
+      ),
     );
   }
 }
