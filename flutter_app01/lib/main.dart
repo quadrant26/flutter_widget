d0a840536efc61743c97424bebaac23e53523ba5
quadrant26 <quadrant26@163.com>
2019-09-23 18:14:07 +0800

    flutter_localizations

--- flutter_app01/lib/main.dart
+++ flutter_app01/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './pages/Tabs.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import './routes/Routes.dart';
 
@@ -12,8 +13,15 @@ class MyApp extends StatelessWidget{
     // TODO: implement build
     return MaterialApp(
       // home: Tabs(),
+      localizationsDelegates: [                       // <-- 添加
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [                             // <-- 添加语言包
+          const Locale('zh', 'CH'),
+      ],
       debugShowCheckedModeBanner: false,
-      initialRoute: '/buttons', // 初始化需要加载的路由
+      initialRoute: '/', // 初始化需要加载的路由
       onGenerateRoute: onGenerateRoute
     );
   }
