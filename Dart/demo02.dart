c34ec3946e84a6926e04d4f875bba02747139e9a
quadrant26 <quadrant26@163.com>
2019-09-24 17:09:03 +0800

    变量和常量

---
+++ Dart/demo02.dart
@@ -0,0 +1,20 @@
+
+// 没有返回值
+void main (){
+  
+  var str = "123";
+
+  String str1 = "hello, World";
+
+  int number = 123;
+
+  print(number);
+
+  final str3 = "234";
+
+  const PI = 3.14159;
+  print(str3);
+
+
+
+}
